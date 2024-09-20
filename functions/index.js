d7ff2c0d561b82c71f2ad1cf89e31fe2bd64463a
Max Breslauer-Friedman <maxnbf@Maxs-MacBook-Pro-2.local>
2024-09-20 18:14:20 -0400

    makes function download images and return image data

--- functions/index.js
+++ functions/index.js
@@ -13,7 +13,48 @@ const logger = require("firebase-functions/logger");
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = onRequest((request, response) => {
+const admin = require("firebase-admin");
+const {getStorage} = require("firebase-admin/storage");
+
+admin.initializeApp();
+const db = admin.firestore();
+
+exports.getAds = onRequest(async (request, response) => {
   logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+
+  // Your Firebase Storage bucket name
+  const bucketName = "adup-b77b2.appspot.com";
+  const bucket = getStorage().bucket(bucketName);
+
+  // Get the first 5 documents in ads collection,
+  // where field shouldBeShown is true
+  const snapshot = await db
+      .collection("ads")
+      .limit(5)
+      .where("shouldBeShown", "==", true)
+      .get();
+
+  // Extract imagePath
+  const adImagePaths = snapshot.docs.map((doc) => doc.data()["imagePath"]);
+
+  // Fetch data for all the files in parallel
+  const imagePromises = adImagePaths.map(async (imagePath) => {
+    const image = bucket.file(imagePath);
+    const [imageData] = await image.download();
+    return {
+      imagePath,
+      imageData: imageData.toString("base64"),
+    };
+  });
+
+  // Wait for all files to be fetched
+  const imagesData = await Promise.all(imagePromises);
+
+  // Set the appropriate content type for JSON response
+  response.setHeader("Content-Type", "application/json");
+
+  // Return the list of file data
+  return response.status(200).send({
+    images: imagesData, // Send the file paths and data
+  });
 });
